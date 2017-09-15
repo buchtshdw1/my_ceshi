ce8f4d3847ae8a5ca5738e695fe725cc2e7549ff
buchten <stbucht@gmail.com>
2017-09-15 12:39:19 -0500

    Create HelloGit.py

---
+++ HelloGit.py
@@ -0,0 +1,11 @@
+class MyGit():
+    def __init__(self):
+        self.greet="wow!"
+    def say_sth(self,content):
+        print(content)
+        
+        
+me=MyGit()
+me.say_sth(me.greet)
+me.say_sth("where do you work?")
+    
